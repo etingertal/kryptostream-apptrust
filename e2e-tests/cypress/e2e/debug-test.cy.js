7ce74d582a3d70a600ca52b2587def2189bb8f03
noame <noame@jfrog.com>
2025-08-26 16:25:14 +0300

    cypress debug scripts

---
+++ e2e-tests/cypress/e2e/debug-test.cy.js
@@ -0,0 +1,9 @@
+describe('Debug Test', () => {
+  it('should pass a simple test', () => {
+    expect(1 + 1).to.equal(2)
+  })
+
+  it('should handle basic math', () => {
+    expect(2 * 3).to.equal(6)
+  })
+})
