86f28eefb663f0f0aba470ea63851e21ff8499a8
Sela Lerer <selal@jfrog.com>
2025-09-06 13:11:28 +0300

    OPS-12 Port forwarding script.

---
+++ portforward.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo "Starting port-forwarding for ArgoCD, Quotopia UI, and QuoteOfDay..."
+
+# Function to run a port-forward command and restart it if it fails
+run_port_forward() {
+  local service_name=$1
+  local local_port=$2
+  local service_port=$3
+  local namespace=$4
+
+  while true; do
+    echo "Starting port-forward for $service_name on port $local_port..."
+    kubectl -n "$namespace" port-forward "$service_name" "$local_port":"$service_port"
+
+    # The command above will exit if the connection is broken (e.g., pod replaced).
+    echo "Port-forward for $service_name exited. Restarting in 2 seconds..."
+    sleep 2
+  done
+}
+
+# Run each port-forward command in the background
+run_port_forward svc/argocd-server 8080 443 argocd &
+run_port_forward service/quotopia-ui-service 9000 80 quotopia &
+run_port_forward service/quoteofday-service 8001 8001 quotopia &
+
+echo "All port-forwarding processes are running in the background. Press Ctrl+C to stop them."
+
+wait
+
+
